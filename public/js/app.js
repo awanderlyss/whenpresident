688a70bf84d6e9d31f324ebe3e217ee3ea7be5ce
awanderlyss <alyssa.felix7@gmail.com>
2017-01-19 19:45:32 -0500

    Work completed in class

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,62 @@
-angular
-  .module("whenPresident", [
-    "ui.router",
-    "ngResource"
-  ])
+(function(){
+
+  angular
+    .module("whenPresident", [
+      "ui.router",
+      "ngResource"
+    ])
+    .config([
+      "$stateProvider",
+      Router
+    ])
+    .factory("Candidate", [
+      "$resource",
+      CandidateFunction
+    ])
+    .controller("indexCtrl", [
+      "Candidate",
+      indexCtrlFunction
+    ])
+    .controller("showCrtl", [
+      "$stateParams",
+      "Candidate",
+      showCrtlFunction
+    ]);
+
+    function Router($stateProvider){
+      $stateProvider
+        .state("welcome", {
+          url: "/",
+          templateUrl: "/assets/js/ng-views/welcome.html"
+        })
+        .state("index", {
+          url: "/candidates",
+          templateUrl: "/assets/js/ng-views/index.html",
+          controller: "indexCtrl",
+          controllerAs: "vm"
+        })
+        .state("show", {
+          url: "/candidates/:name",
+          templateUrl: "/assets/js/ng-views/show.html",
+          controller: "showCrtl",
+          controllerAs: "vm"
+        });
+    }
+
+    function CandidateFunction($resource) {
+      return $resource("/api/candidates/:name", {}, {
+        update:{
+          method: "PUT"
+        }
+      });
+    }
+
+    function indexCtrlFunction(Candidate){
+      this.candidates = Candidate.query();
+    }
+
+    function showCrtlFunction($stateParams, Candidate){
+      this.candidate = Candidate.get({name: $stateParams.name});
+    }
+
+})();
